073097b1fe8dd0af7cd97c817584abc0f92f6303
Titanf55 <144533399+Titanf55@users.noreply.github.com>
2024-06-19 15:04:51 +0530

    Update Q4.py

--- MLassignment1/Q4.py
+++ MLassignment1/Q4.py
@@ -15,6 +15,45 @@ try:
  name=int(input("enter name"))
 except (TypeError ,ValueError):
   print('hola como estas')
+ #all these will produce errors _ _ _ _ _ _ _ _ _ _  
+                                 #enter atleat 2 charac like enter 12k3j output kj 
+import re
+
+def get_alpha_string(prompt):
+    while True:
+        user_input = input(prompt)
+        # Use regular expression to match only alphabetic characters
+        alpha_string = re.sub(r'[^a-zA-Z]', '', user_input)
+        
+        if len(alpha_string) >= 2:
+            return alpha_string
+        else:
+            print("Please enter at least 2 letter name.")
+
+                                  #enter 12k3j output kj
+# def get_alpha_string(prompt):
+#     while True:
+#         user_input = input(prompt)
+#         alpha_string = ''.join(filter(str.isalpha, user_input))  # Filter out non-alphabetic characters
+        
+#         if len(alpha_string) >= 2:
+#             return alpha_string
+#         else:
+#             print("Please enter at least two alphabetic characters.")
+
+                                  #enter 12k3j it will take if has atleast 2 charc
+# def get_alpha_string(prompt):
+#     while True:
+#         user_input = input(prompt)
+#         alpha_count = sum(1 for char in user_input if char.isalpha())
+        
+#         if alpha_count >= 2:
+#             return user_input
+#         else:
+#             print("Please enter a string with at least two alphabetic characters.")
+
+alpha_string = get_alpha_string("Enter a string with only alphabetic characters: ")
+print(f"hola {alpha_string} como estas!")
 
 
         
